0e9eb1e62ce188d3dd711b82ec12e0bdc1666beb
Rasmus Andersson <rasmus@notion.se>
2009-08-31 17:14:23 +0200

    sshfs mount of hal

--- launchd/fuse.sshfs.mount.sh
+++ launchd/fuse.sshfs.mount.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
+# See http://code.google.com/p/macfuse/wiki/OPTIONS for options
 mkdir -p /Volumes/hal
-/usr/bin/sshfs hal:/ /Volumes/hal -f -o reconnect,transform_symlinks,idmap=user,jail_symlinks,volname=hal
+/opt/local/bin/sshfs \
+	hal.hunch.se:/ \
+	/Volumes/hal -f -C \
+	-o reconnect \
+	-o idmap=user,transform_symlinks,jail_symlinks,noappledouble \
+	-o volname=hal,fsid=0x1337
+	#-o iconpath=$HOME/Library/Scripts/some.icon
