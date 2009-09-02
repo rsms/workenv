d5dea1cb7cababa02594fb56c2d1e9ced063a049
Rasmus Andersson <rasmus@notion.se>
2009-09-02 16:26:13 +0200

    sshfs launchd script updated

--- launchd/fuse.sshfs.mount.sh
+++ launchd/fuse.sshfs.mount.sh
@@ -3,8 +3,10 @@
 mkdir -p /Volumes/hal
 /opt/local/bin/sshfs \
 	hal.hunch.se:/ \
-	/Volumes/hal -f -C \
-	-o reconnect \
+	/Volumes/hal -f \
+	-o reconnect,compression=yes \
 	-o idmap=user,transform_symlinks,jail_symlinks,noappledouble \
-	-o volname=hal,fsid=0x1337
+	-o volname=hal
 	#-o iconpath=$HOME/Library/Scripts/some.icon
+
+
