d33c8a5b0079b1a8a0b4f52cc3870f1e5b0ef8ab
Rasmus Andersson <rasmus@notion.se>
2010-08-16 01:04:44 +0200

    basics + OS X basic text commands (Cmd+-> = move to end of line, etc)

--- terminal/emacs.el
+++ terminal/emacs.el
@@ -1,15 +1,61 @@
+(add-to-list 'load-path "~/.emacs.d/")
+
+;; General
+(custom-set-variables
+ '(indent-tabs-mode nil)
+ '(standard-indent 2)
+ '(tab-width 2)
+ '(default-frame-alist (quote ((cursor-color . "Red") (cursor-type . bar))))
+)
+
+;; Key bindings (On OS X, s=Cmd)
+(global-set-key (kbd "s-}") 'next-buffer)     ; OS X-isch "next tab"
+(global-set-key (kbd "s-{") 'previous-buffer) ; OS X-isch "previous tab"
+(global-set-key (kbd "s-<right>") 'end-of-line)
+(global-set-key (kbd "s-<left>") 'beginning-of-line)
+(global-set-key (kbd "s-<up>") 'beginning-of-buffer)
+(global-set-key (kbd "s-<down>") 'end-of-buffer)
+
+;; Theme
+(custom-set-faces
+ '(default ((t (:inherit nil :stipple nil :background "#171717" :foreground "#ebebeb" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight light :height 120 :width normal :foundry "apple" :family "M+_1m"))))
+ ;;'(c++-mode-default ((t (:inherit autoface-default :foreground "#ebebeb" :height 130 :family "M+ 1m"))) t)
+ ;;'(completion-list-mode-default ((t (:inherit autoface-default))) t)
+ ;;'(cursor ((t (:background "red"))))
+ '(font-lock-comment-face
+   ((((class color) (min-colors 88) (background dark)) (:foreground "#888"))))
+ '(font-lock-constant-face
+   ((((class color) (min-colors 88) (background dark)) (:foreground "#f57aab"))))
+ '(font-lock-doc-face
+   ((t (:inherit font-lock-comment-face))))
+ '(font-lock-function-name-face
+   ((((class color) (min-colors 88) (background dark)) (:background "#232f36" :foreground "#59f5c6"))))
+ '(font-lock-keyword-face
+   ((((class color) (min-colors 88) (background dark)) (:foreground "#5ecefc"))))
+ '(font-lock-string-face
+   ((((class color) (min-colors 88) (background dark)) (:background "#1a2216" :foreground "#86ad6c"))))
+ '(font-lock-type-face ((((class color) (min-colors 88) (background dark)) (:foreground "LightSalmon"))))
+ '(highlight ((((class color) (min-colors 88) (background dark)) (:background "yellow" :foreground "black"))))
+ ;;'(js-mode-default ((t (:inherit autoface-default :background "#191919" :foreground "#ededed"))) t)
+ '(js2-external-variable-face ((t (:foreground "#be8971"))))
+ '(js2-function-param-face ((t (:foreground "#e8a970"))))
+ '(js2-jsdoc-value-face ((t (:foreground "#e8a970"))))
+ '(region ((((class color) (min-colors 88) (background dark)) (:background "#334466"))))
+ '(text-mode-default
+   ((t (:inherit autoface-default :stipple nil :strike-through nil :underline nil :slant normal :weight normal :height 130 :width normal :family "Lucida Grande"))))
+ '(tool-bar ((t (:box (:line-width 1 :style released-button))))))
+
+;; auto-complete
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d//ac-dict")
+(ac-config-default)
+
+;; JavaScript
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (autoload 'espresso-mode "espresso")
 (add-to-list 'auto-mode-alist '("\\.json$" . espresso-mode))
 
-; auto-complete
-(add-to-list 'load-path "~/.emacs.d/")
-(require 'auto-complete-config)
-(add-to-list 'ac-dictionary-directories "~/.emacs.d//ac-dict")
-(ac-config-default)
-
-; js2 stuff
 (defun my-js2-indent-function ()
   (interactive)
   (save-restriction
