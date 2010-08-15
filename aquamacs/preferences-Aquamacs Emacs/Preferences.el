597590420db191f9dabc22c239038672706b69ed
Rasmus Andersson <rasmus@notion.se>
2010-08-15 15:39:48 +0200

    Emacs & Aquamacs prefs

---
+++ aquamacs/preferences-Aquamacs Emacs/Preferences.el
@@ -0,0 +1,6 @@
+;; This is the Aquamacs Preferences file.
+;; Add Emacs-Lisp code here that should be executed whenever
+;; you start Aquamacs Emacs. If errors occur, Aquamacs will stop
+;; evaluating this file and print errors in the *Messags* buffer.
+;; Use this file in place of ~/.emacs (which is loaded as well.)
+
---
+++ aquamacs/preferences-Aquamacs Emacs/customizations.el
@@ -0,0 +1,39 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(aquamacs-additional-fontsets nil t)
+ '(aquamacs-autoface-mode nil)
+ '(aquamacs-customization-version-id 208 t)
+ '(aquamacs-set-creator-codes-after-writing-files nil)
+ '(aquamacs-tool-bar-user-customization nil t)
+ '(c-basic-offset 2)
+ '(default-frame-alist (quote ((cursor-type . box) (vertical-scroll-bars . right) (internal-border-width . 0) (fringe) (modeline . t) (mouse-color . "black") (background-mode . dark) (tool-bar-lines . 1) (menu-bar-lines . 1) (right-fringe . 10) (left-fringe . 2) (cursor-color . "Red") (background-color . "#171717") (foreground-color . "#ebebeb") (font . "-apple-M+_1m-medium-normal-normal-*-12-*-*-*-m-0-iso10646-1") (fontsize . 0) (font-backend ns))))
+ '(indent-tabs-mode nil)
+ '(ns-tool-bar-display-mode (quote labels) t)
+ '(ns-tool-bar-size-mode (quote small) t)
+ '(smart-frame-positioning-enforce nil nil (smart-frame-positioning))
+ '(smart-frame-positioning-mode nil)
+ '(standard-indent 2)
+ '(tab-width 2)
+ '(visual-line-mode nil t))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "#171717" :foreground "#ebebeb" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight light :height 120 :width normal :foundry "apple" :family "M+_1m"))))
+ '(aquamacs-variable-width ((((type ns)) (:stipple nil :strike-through nil :underline nil :slant normal :weight light :height 120 :width normal :family "M+_1c"))))
+ '(c++-mode-default ((t (:inherit autoface-default :foreground "#ebebeb" :height 130 :family "M+ 1m"))) t)
+ '(completion-list-mode-default ((t (:inherit autoface-default))) t)
+ '(font-lock-comment-face ((((class color) (min-colors 88) (background dark)) (:foreground "#888"))))
+ '(font-lock-constant-face ((((class color) (min-colors 88) (background dark)) (:foreground "#f57aab"))))
+ '(font-lock-function-name-face ((((class color) (min-colors 88) (background dark)) (:background "#232f36" :foreground "#59f5c6"))))
+ '(font-lock-string-face ((((class color) (min-colors 88) (background dark)) (:background "#1a2216" :foreground "#86ad6c"))))
+ '(font-lock-type-face ((((class color) (min-colors 88) (background dark)) (:foreground "LightSalmon"))))
+ '(highlight ((((class color) (min-colors 88) (background dark)) (:background "yellow" :foreground "black"))))
+ '(js-mode-default ((t (:inherit autoface-default :background "#191919" :foreground "#ededed"))) t)
+ '(region ((((class color) (min-colors 88) (background dark)) (:background "#334466"))))
+ '(text-mode-default ((t (:inherit autoface-default :stipple nil :strike-through nil :underline nil :slant normal :weight normal :height 130 :width normal :family "Lucida Grande"))))
+ '(tool-bar ((t (:box (:line-width 1 :style released-button))))))
